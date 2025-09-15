30b8415524b4a055bf0408fab9e2045ec5f2b187
Andrade <dudadeandrade13@gmail.com>
2025-09-15 14:41:07 -0300

    feat: cria a tabela de artistas

---
+++ queries/01-insert-artists.sql
@@ -0,0 +1,12 @@
+-- Query para a criação da tabela --
+CREATE TABLE artistas (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  tipo VARCHAR(20) NOT NULL,
+  membros INT,
+  genero_musical VARCHAR (40),
+  ano_lancamento INT, 
+  pais_origem VARCHAR (50),
+  musica_famosa VARCHAR (50),
+  ativo BOOLEAN
+);
